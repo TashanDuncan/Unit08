494a94caca0a6ec267845fc0032cbd9137759f82
viewtifult <tashanduncan1994@gmail.com>
2020-12-11 12:40:26 +0000

    fetch data from API

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// ------------------------------------------
+//  FETCH FUNCTIONS
+// ------------------------------------------
+
+function fetchData(url) {
+    return fetch(url)
+             .then(checkStatus)  
+             .then(res => res.json())
+             .catch(error => console.log('Looks like there was a problem!', error))
+  }
+
+  fetchData('https://randomuser.me/api/')
+
+
+// ------------------------------------------
+//  HELPER FUNCTIONS
+// ------------------------------------------
+
+function checkStatus(response) {
+    if (response.ok) {
+      return Promise.resolve(response);
+    } else {
+      return Promise.reject(new Error(response.statusText));
+    }
+  }
