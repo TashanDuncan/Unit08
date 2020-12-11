b38641c48826f6031e056f2f9ec0da7c19216c56
viewtifult <tashanduncan1994@gmail.com>
2020-12-11 21:03:20 +0000

    modal data function created

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@ const randomUsersURL = 'https://randomuser.me/api/?results=12';
 const employees = document.querySelector('.employees');
 const modal = document.getElementById('modal');
 const modalClose = document.querySelector('.modal-close');
-
+const modalContent = document.querySelector('.modal-content');
+let employeeArray
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
@@ -18,17 +19,19 @@ function fetchData(url) {
              .catch(error => console.log('Looks like there was a problem!', error))
   }
 
-  fetchData(randomUsersURL)
+ fetchData(randomUsersURL)
     .then(data => { 
         const results = data.results;
-
+        let userNumber = 0;
         for (const user of results){
+            user.userNumber = userNumber;
             console.log(user)
             generateEmployee(user)
+            userNumber++
         }
         console.log(results);
+        employeeArray = results;
     })
-
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
@@ -45,6 +48,7 @@ function checkStatus(response) {
   function generateEmployee(data) {
     const createLI = document.createElement("LI");
     createLI.className = "employee-container";
+    createLI.dataset.index = data.userNumber
     const user = `
     <img class="employee-image" src="${data.picture.large}">
     <div class="employee-details">
@@ -62,9 +66,28 @@ function checkStatus(response) {
 //  Modal FUNCTIONS
 // ------------------------------------------
 
+function displayModal(index) {
+  const display = `                
+  <img class="modal-avatar" src="${employeeArray[index].picture.large}" />
+  <div class="modal-text-container">
+      <h2 class="modal-name">Haleigh Macchiarella</h2>
+      <p class="modal-email">dtucker@yakitri.edu</p>
+      <p class="modal-city">Chicago</p>
+      <hr/>
+      <p>(593) 364-3249</p>
+      <p class="modal-address">123 Anywhere St, WV 84814</p>
+      <p>Birthday: 01/04/85</p>
+  </div>`
+  modalContent.innerHTML = display;
+}
+
+
 employees.addEventListener('click', (e) => {
   const event = e.target
   if(event !== employees){
+  const card = event.closest('.employee-container')
+  const index = card.getAttribute('data-index')
+  displayModal(index);
   modal.classList.remove('hidden');
   }
 
